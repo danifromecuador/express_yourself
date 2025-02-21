6a0599b68d0d88cb85e72b77ee469a2857e8ed33
dani <danifromecuador@gmail.com>
2025-02-21 03:09:58 -0500

    Create a /expressions GET endpoint

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-const express = require('express')
+const express = require('express');
+const app = express();
 
-const app = express()
-const PORT = process.env.PORT || 4001
+const PORT = process.env.PORT || 4001;
+// Use static server to serve the Express Yourself Website
+app.use(express.static('public'));
+
+// Open a call to `app.get()` below:
+app.get('/expressions', (req, res, next)=>{
+  console.log(req)
+})
 
 app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
-})
+  console.log(`Listening on port ${PORT}`);
+});
